2cc02b2562a15398f85d54cb2639d45aac3e66e1
GibranNahumGonzalezSantamaria <146513259+GibranNahumGonzalezSantamaria@users.noreply.github.com>
2025-01-01 19:24:45 -0600

    Update ExpresionesAritmeticasASM.java
    + Expresión Aritmética a través de ficheros.
    + Formato
    + Intento de Decimales

--- ExpresionesAritmeticasASM.java
+++ ExpresionesAritmeticasASM.java
@@ -368,9 +368,6 @@ public class ExpresionesAritmeticasASM {
         String parteDecimalFormateada = String.format("%03d", parteDecimal);
 
         // Retorna el formato ASM deseado
-        return String.format("%s DW %s\n    %s_D DW %s",
-                variable, parteEnteraFormateada,
-                variable, parteDecimalFormateada
-        );
+        return String.format("%s\n    %s_D DW %s", parteEnteraFormateada, variable, parteDecimalFormateada);
     }
 }
